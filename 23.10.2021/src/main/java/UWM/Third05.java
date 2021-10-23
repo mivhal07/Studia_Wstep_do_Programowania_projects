75997b0462ac7dc394957df2cd9121dc1f11fda8
mivhal07 <michaldob07@gmail.com>
2021-10-23 22:31:50 +0200

    po pierwszym wykładzie

---
+++ 23.10.2021/src/main/java/UWM/Third05.java
@@ -0,0 +1,10 @@
+package UWM;
+
+public class Third05 {
+
+    public static void main(String[] args) {
+        double yearInSec = 3.156 * (Math.pow(10, 7));
+        double myAgeInSec = 21 * yearInSec;
+        System.out.println(myAgeInSec);
+    }
+}
