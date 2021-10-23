33466560c39da118e34107edc4efd8bb2e3ea4bc
mivhal07 <89939646+mivhal07@users.noreply.github.com>
2021-10-23 19:01:01 +0200

    Add files via upload
    zadania po pierwszym wykładzie

---
+++ 23.10.2021/src/main/java/UWM/Second03.java
@@ -0,0 +1,10 @@
+package UWM;
+
+public class Second03 {
+
+    public static void main(String[] args) {
+        int ageInYears = 21;
+        int ageInDays = ageInYears * 365;
+        System.out.println(ageInDays);
+    }
+}
