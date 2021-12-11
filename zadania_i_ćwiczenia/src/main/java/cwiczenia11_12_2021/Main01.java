97a5542208182c9b4bf3ba59521ae30e9ad0335d
mivhal07 <michaldob07@gmail.com>
2021-12-11 08:53:56 +0100

    ćwiczenia 11.12.2021

---
+++ zadania_i_ćwiczenia/src/main/java/cwiczenia11_12_2021/Main01.java
@@ -0,0 +1,59 @@
+package cwiczenia11_12_2021;
+
+import java.util.Scanner;
+
+public class Main01 {
+
+    public static void main(String[] args) {
+
+        //rozwiązać równanie liniowe A*x+B=0
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź parametr 'A', nie może być zerem");
+        double A = scanner.nextDouble();
+        System.out.println("Wprowadź parametr 'B'");
+        double B = scanner.nextDouble();
+        if (A == 0) {
+            System.out.println("Jak pisałem wcześniej 'A' nie może być zerem");
+        } else {
+            double x = -B / A;
+            System.out.println(String.format("Równanie to: A*x+B=0"));
+            System.out.println(String.format("x = %s", x));
+
+            System.out.println("");
+            System.out.println("Sprawdzenie:");
+            if (x < 0) {
+                System.out.println(String.format("A*x+B=0  =>  %s*x+%s=0  =>  %s*(%s)+%s=0", A, B, A, x, B));
+            } else {
+                System.out.println(String.format("A*x+B=0  =>  %s*x+%s=0  =>  %s*%s+%s=0", A, B, A, x, B));
+            }
+            System.out.println("Sprawdzenie:");
+            System.out.println(String.format("A*x+B=0  =>  %s*x+%s=0  =>  x=-%s/%s  =>  x=%s", A, B, B, A, x));
+        }
+    }
+}
+
+/*
+Schemat blokowy:
+
+ ---------------------
+| Wprowadzenie danych |
+ ---------------------
+        |
+        |
+        v
+ -----------------   TAK    -----------------------            --------
+| jeżeli A = 0    |  --->  |  A nie może być zerem |  ---->   | Koniec |
+ -----------------          -----------------------            --------
+        |
+        |  NIE
+        v
+ -----------------------
+| przekształcenie wzoru |
+ -----------------------
+        |
+        |
+        v
+ ---------------------------
+| Obliczenie i wyświetlenie |
+ ---------------------------
+ */
