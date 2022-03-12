d791e309a6c2cf013942efcac2212b7de9dbfb54
mivhal07 <michaldob07@gmail.com>
2022-03-12 16:44:34 +0100

    wykład 05.03.2022 r.

--- zadania_i_ćwiczenia/src/main/java/wyklad05_03_2022/ninth08.java
+++ zadania_i_ćwiczenia/src/main/java/wyklad05_03_2022/ninth08.java
@@ -1,10 +1,35 @@
 package wyklad05_03_2022;
 
+import java.util.Scanner;
+
 public class ninth08 {
 
     public static void main(String[] args) {
-        int liczba =0;
-        System.out.println("Podaj liczbę całkowitą");
+        int liczba;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę całkowitą, quit kończy program");
+        while (true) {
+            String s = scanner.nextLine();
+            if (s.equals("quit")) {
+                break;
+            }
+            System.out.println("Podaj cyfrą na jaki system mam zmienić liczbę");
+            int syst = scanner.nextInt();
+            liczba = Integer.parseInt(s);
+            System.out.println("Odpowiednik dwójkowy:");
+            do_binar(liczba, syst);
+            System.out.println("");
+            System.out.println("Podaj liczbę całkowitą, quit kończy program");
+        }
 
+    }
+
+    public static void do_binar(int n, int a) {//3103
+        int r = 0;
+        r = n % a;
+        if (n >= a) {
+            do_binar(n / a, a);
+        }
+        System.out.print(0 + r);
     }
 }
