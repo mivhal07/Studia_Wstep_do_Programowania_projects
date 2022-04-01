a5bb79ad441375e870e663922c7d682fe5955cb8
mivhal07 <michaldob07@gmail.com>
2022-04-01 16:25:24 +0200

    ćwiczenia

--- zadania_i_ćwiczenia/src/main/java/wyklad19_03_2022/fourteenth02.java
+++ zadania_i_ćwiczenia/src/main/java/wyklad19_03_2022/fourteenth02.java
@@ -1,9 +1,26 @@
 package wyklad19_03_2022;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
 public class fourteenth02 {
 
     public static void main(String[] args) {
-
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj miesiąc i rok w formacie 'MM-RRRR', a ja pokażę Ci, ile dni jest w tym miesiącu");
+        String s = scanner.nextLine();
+        showDays(s);
     }
+
+    public static void showDays(String s) {
+        String[] split = s.split("-");
+        int i1 = Integer.parseInt(split[0]);
+        int i2 = Integer.parseInt(split[1]);
+        Calendar kalendarz = new GregorianCalendar(i2, i1, 0);
+        int dniMiesiac = kalendarz.getActualMaximum(Calendar.DAY_OF_MONTH);
+        System.out.println("Ilość dni w podanym miesiącu: " + dniMiesiac);
+    }
+
+
 }
